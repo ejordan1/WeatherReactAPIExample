602e6554bf5a46babcad36811140f8bb978183bf
Kelar Crisp <kelcrisp007@icloud.com>
2019-09-17 21:07:20 -0700

    setup to pull information from openweathermapAPI, just need to design the application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+import Title from './components/Title';
+class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const API_KEY = 'f7c5ea17c630e570c8b7fea4b66e2675';
+    const API_CALL = await fetch(`https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${API_KEY}2`);
+    const data = await API_CALL.json();
+  }
+  render() {
+    return (
+
+      <div className="App">
+        <Title />
+        <Form getWeather={this.getWeather} />
+      </div>
+    );
+  }
 }
 
 export default App;
